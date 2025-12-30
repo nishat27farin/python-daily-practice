db401ace01fb656a5a88d238f3f67136f729acf2
Nishat Shama <nshama@idare.io>
2025-12-30 16:19:26 +0600

    Variable Learning

---
+++ basics/004_variable.py
@@ -0,0 +1,8 @@
+tester_name = "Nishat"
+tester_role = "SQA Engineer"
+experience_year = 2
+project_name = "Billing System"
+city = "Dhaka"
+
+print(tester_name, tester_role, experience_year, city)
+print(project_name)
